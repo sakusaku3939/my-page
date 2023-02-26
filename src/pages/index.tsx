764921120d75189fc592b4d2e3207062cec2460d
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-02-26 23:32:05 +0900

    Rename

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import {Noto_Sans_JP} from 'next/font/google'
 import styles from '@/styles/index.module.css'
 
-const inter = Noto_Sans_JP({weight: "500", subsets: ['latin']})
+const font = Noto_Sans_JP({weight: "500", subsets: ['latin']})
 
 export default function Home() {
     return (
@@ -12,7 +12,7 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <main className={inter.className}>
+            <main className={font.className}>
                 <p>テスト</p>
             </main>
         </>
