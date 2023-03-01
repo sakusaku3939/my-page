7e0d1664ae37ba75a6a7e64b957a3e8812a5382b
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-03-02 03:11:51 +0900

    Add sns links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,22 @@
-import Head from 'next/head'
-import {Noto_Sans_JP} from 'next/font/google'
-import styles from '@/styles/index.module.css'
+import Head from "next/head";
+import { Noto_Sans_JP } from "next/font/google";
+import styles from "@/styles/index.module.css";
+import SnsList from "@/components/sns-list";
 
-const font = Noto_Sans_JP({weight: "500", subsets: ['latin']})
+const font = Noto_Sans_JP({ weight: "500", subsets: ["latin"] });
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Profile | Aokiti</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <main className={font.className}>
-                <p>テスト</p>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Profile | Aokiti</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={font.className}>
+        <h1 className={styles.title}>Aokiti</h1>
+        <SnsList />
+      </main>
+    </>
+  );
 }
