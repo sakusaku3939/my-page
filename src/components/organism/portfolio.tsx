8912a85bde4c8800b1b417f416b0aa3405fbb562
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-03-05 13:16:15 +0900

    Refactoring

--- src/components/organism/portfolio.tsx
+++ src/components/organism/portfolio.tsx
@@ -1,4 +1,5 @@
 import portfolio from "@/styles/portfolio.module.css";
+import common from "@/styles/common.module.css";
 import "@splidejs/react-splide/css";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import Link from "next/link";
@@ -12,65 +13,63 @@ const Portfolio = () => {
         autoWidth: true
       }}>
         <SplideSlide>
-          <div className={`${portfolio.card} ${portfolio.skin}`}>
-            <div className={portfolio.date}>2021.1.1</div>
-            <div className={portfolio.image}></div>
-            <div className={portfolio.textBox}>
-              <div className={portfolio.title}>test</div>
-              <ul className={portfolio.tag}>
-                <li>test</li>
-                <li>test2</li>
-              </ul>
-              <div className={portfolio.overview}>aaaaaa</div>
-            </div>
-          </div>
+          <Post date="2021.1.1"
+                imageUrl=""
+                title="test1"
+                tag={["test", "test2"]}
+                overview="aaaaa" />
         </SplideSlide>
         <SplideSlide>
-          <div className={`${portfolio.card} ${portfolio.skin}`}>
-            <div className={portfolio.date}>2021.1.1</div>
-            <div className={portfolio.image}></div>
-            <div className={portfolio.textBox}>
-              <div className={portfolio.title}>test</div>
-              <ul className={portfolio.tag}>
-                <li>test</li>
-                <li>test2</li>
-              </ul>
-              <div className={portfolio.overview}>bbbbbb</div>
-            </div>
-          </div>
+          <Post date="2021.1.1"
+                imageUrl=""
+                title="test2"
+                tag={["test", "test2"]}
+                overview="bbbbb" />
         </SplideSlide>
         <SplideSlide>
-          <div className={`${portfolio.card} ${portfolio.skin}`}>
-            <div className={portfolio.date}>2021.1.1</div>
-            <div className={portfolio.image}></div>
-            <div className={portfolio.textBox}>
-              <div className={portfolio.title}>test</div>
-              <ul className={portfolio.tag}>
-                <li>test</li>
-                <li>test2</li>
-              </ul>
-              <div className={portfolio.overview}>cccccc</div>
-            </div>
-          </div>
+          <Post date="2021.1.1"
+                imageUrl=""
+                title="test3"
+                tag={["test", "test2"]}
+                overview="ccccc" />
         </SplideSlide>
         <SplideSlide>
-          <div className={`${portfolio.card} ${portfolio.skin}`}>
-            <div className={portfolio.date}>2021.1.1</div>
-            <div className={portfolio.image}></div>
-            <div className={portfolio.textBox}>
-              <div className={portfolio.title}>test</div>
-              <ul className={portfolio.tag}>
-                <li>test</li>
-                <li>test2</li>
-              </ul>
-              <div className={portfolio.overview}>cccccc</div>
-            </div>
-          </div>
+          <Post date="2021.1.1"
+                imageUrl=""
+                title="test4"
+                tag={["test", "test2"]}
+                overview="ddddd" />
         </SplideSlide>
       </Splide>
     </div>
     <div className={portfolio.button}>
       <Link href="">もっと見る</Link>
+    </div>
+  </>;
+};
+
+type PostProps = {
+  date: string
+  imageUrl: string
+  title: string
+  tag: string[]
+  overview: string
+};
+
+const Post = ({ date, imageUrl, title, tag, overview }: PostProps) => {
+  return <>
+    <div className={`${portfolio.card} ${portfolio.skin}`}>
+      <div className={portfolio.date}>{date}</div>
+      <div className={portfolio.image}>{imageUrl}</div>
+      <div className={portfolio.textBox}>
+        <div className={portfolio.title}>{title}</div>
+        <ul className={common.tag}>
+          {tag.map((value: string, key: number) => (
+            <li key={key}>{value}</li>
+          ))}
+        </ul>
+        <div className={portfolio.overview}>{overview}</div>
+      </div>
     </div>
   </>;
 };
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Noto_Sans_JP } from "next/font/google";
-import styles from "@/styles/index.module.css";
+import index from "@/styles/index.module.css";
 import SnsList from "@/components/molecule/sns-list";
 import Timeline from "@/components/organism/timeline";
 import Qualification from "@/components/organism/qualification";
@@ -17,17 +17,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={font.className}>
-        <section className={styles.header}>
-          <div className={styles.title}>Aokiti</div>
-          <div className={styles.subtitle}>Keio Univ(SFC) B1 / Web, Android</div>
+        <section className={index.header}>
+          <div className={index.title}>Aokiti</div>
+          <div className={index.subtitle}>Keio Univ(SFC) B1 / Web, Android</div>
           <SnsList />
         </section>
-        <section className={styles.main}>
-          <h1 className={styles.h1}>制作物</h1>
+        <section className={index.main}>
+          <h1 className={index.h1}>制作物</h1>
           <Portfolio />
-          <h1 className={styles.h1}>経歴</h1>
+          <h1 className={index.h1}>経歴</h1>
           <Timeline />
-          <h1 className={styles.h1}>取得資格</h1>
+          <h1 className={index.h1}>取得資格</h1>
           <Qualification />
         </section>
       </main>
