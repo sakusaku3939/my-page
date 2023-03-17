f96334f62abdc2574be39326b13abe929ffeb9c8
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-03-18 03:09:27 +0900

    Add post contents & Modify card design

--- src/components/organism/PostsList/PostsList.tsx
+++ src/components/organism/PostsList/PostsList.tsx
@@ -7,26 +7,40 @@ import Link from "next/link";
 const PostsList = () => {
   return <>
     <div className={portfolio.wrapper}>
-      <Post date="2021.1.1"
-            imageUrl=""
-            title="test1"
-            tag={["test", "test2"]}
-            overview="aaaaa" />
-      <Post date="2021.1.1"
-            imageUrl=""
-            title="test2"
-            tag={["test", "test2"]}
-            overview="bbbbb" />
-      <Post date="2021.1.1"
-            imageUrl=""
-            title="test3"
-            tag={["test", "test2"]}
-            overview="ccccc" />
-      <Post date="2021.1.1"
-            imageUrl=""
-            title="test4"
-            tag={["test", "test2"]}
-            overview="ddddd" />
+      <Splide options={{
+        type: "loop",
+        focus: "center",
+        autoWidth: true
+      }}>
+        <SplideSlide>
+          <Post date="2021.10.25"
+                imageUrl="/posts/presc/thumbnail.jpg"
+                title="Presc"
+                tag={["Android / iOS", "Flutter", "Dart"]}
+                overview="音声認識により、どこまで読んだかが分かるプレゼンテーション用原稿表示アプリ" />
+        </SplideSlide>
+        <SplideSlide>
+          <Post date="2021.2.18"
+                imageUrl="/posts/tkg-beacon/thumbnail.jpg"
+                title="TKG Beacon"
+                tag={["Android", "Kotlin", "AltBeacon", "Firebase"]}
+                overview="ビーコンを利用した、生徒と先生の位置情報共有Androidアプリ" />
+        </SplideSlide>
+        <SplideSlide>
+          <Post date="2021.1.1"
+                imageUrl="/posts/deepl-android/thumbnail.jpg"
+                title="OSS DeepL Android"
+                tag={["Android", "Kotlin", "OSS"]}
+                overview="DeepL翻訳のクライアントアプリが欲しかったために作った非公式Androidアプリ" />
+        </SplideSlide>
+        <SplideSlide>
+          <Post date="2022.3.14"
+                imageUrl="/posts/like-button/thumbnail.jpg"
+                title="いいねボタン"
+                tag={["HTML / CSS / JavaScript", "Vue.js", "Firebase"]}
+                overview="プレゼン発表中にリアルタイムで「いいね！」が送れるシステム" />
+        </SplideSlide>
+      </Splide>
     </div>
     <div className={portfolio.button}>
       <Link href="">もっと見る</Link>
@@ -46,10 +60,10 @@ const Post = ({ date, imageUrl, title, tag, overview }: PostProps) => {
   return <>
     <div className={`${portfolio.card} ${portfolio.skin}`}>
       <div className={portfolio.date}>{date}</div>
-      <div className={portfolio.image}>{imageUrl}</div>
+      <div className={portfolio.image} style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className={portfolio.textBox}>
         <div className={portfolio.title}>{title}</div>
-        <ul className={common.tag}>
+        <ul className={`${common.tag} ${portfolio.tag}`}>
           {tag.map((value: string, key: number) => (
             <li key={key}>{value}</li>
           ))}
