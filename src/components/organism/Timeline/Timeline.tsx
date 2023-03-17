6316b477b70a5a5719549e15c117bc65e0b631bb
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-03-18 03:55:56 +0900

    Add icon to list of timeline

--- src/components/organism/Timeline/Timeline.tsx
+++ src/components/organism/Timeline/Timeline.tsx
@@ -1,6 +1,6 @@
 import timeline from "@/components/organism/Timeline/Timeline.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faUpRightFromSquare, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { ReactNode } from "react";
 import Link from "next/link";
 
@@ -20,18 +20,22 @@ const Timeline = ({ children }: TimeLineProps) => {
 
 type TimeLineItemProps = {
   date: string
+  icon: IconDefinition
   title: string
   children?: ReactNode
 };
 
-const TimeLineItem = ({ date, title, children }: TimeLineItemProps) => {
+const TimeLineItem = ({ date, icon, title, children }: TimeLineItemProps) => {
   return <>
     <li>
       <details className={timeline.accordion}>
         <summary>
           <div className={timeline.titleWrapper}>
             <div className={timeline.date}>{date}</div>
-            <div className={timeline.title}>{title}</div>
+            <div className={timeline.title}>
+              <FontAwesomeIcon className={timeline.icon} icon={icon} />
+              <span>{title}</span>
+            </div>
             {children && <div className={timeline.arrow} />}
           </div>
         </summary>
@@ -49,7 +53,7 @@ type TimeLineLinkProps = {
 const TimeLineLink = ({ description, href }: TimeLineLinkProps) => {
   return <>
     <Link className={timeline.link} href={href} target="_blank">
-      <span><FontAwesomeIcon icon={faUpRightFromSquare} /></span>
+      <FontAwesomeIcon className={timeline.icon} icon={faUpRightFromSquare} />
       {"  " + description}
     </Link>
   </>;
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -4,6 +4,7 @@ import SnsList from "@/components/molecule/SnsList/SnsList";
 import { Qualification, QualificationItem } from "@/components/organism/Qualification/Qualification";
 import PostsList from "@/components/organism/PostsList/PostsList";
 import { Timeline, TimeLineItem, TimeLineLink } from "@/components/organism/Timeline/Timeline";
+import { faGraduationCap, faBriefcase, faTrophy } from "@fortawesome/free-solid-svg-icons";
 
 export default function Profile() {
   return (
@@ -24,8 +25,8 @@ export default function Profile() {
 
           <h1 className={index.h1}>経歴</h1>
           <Timeline>
-            <TimeLineItem date="2019.4 ~" title="東京都立新宿山吹高校（定時制） 情報科" />
-            <TimeLineItem date="2019.8 ~" title="ロボコンチーム  BWW 所属">
+            <TimeLineItem date="2019.4 ~" icon={faGraduationCap} title="東京都立新宿山吹高校（定時制） 情報科" />
+            <TimeLineItem date="2019.8 ~" icon={faBriefcase} title="ロボコンチーム  BWW 所属">
               高1〜高3の間に世界最大級のロボコン FRC（FIRST Robotics
               Competition）に出場するチーム「BWW」に設立メンバーとして活動しました。ソフトウェアや公式Webサイト作成、ブログやTwitter、YouTubeなどでの広報活動を行いました。
               <TimeLineLink
@@ -37,7 +38,7 @@ export default function Profile() {
                 href="https://www.youtube.com/@BWW8231"
               />
             </TimeLineItem>
-            <TimeLineItem date="2021.10" title="アプリ甲子園2021  第3位・技術賞">
+            <TimeLineItem date="2021.10" icon={faTrophy} title="アプリ甲子園2021  第3位・技術賞">
               アプリ甲子園2021にて、プレゼン原稿表示アプリ「Presc」を発表しました。第3位と技術賞を頂きました。
               <TimeLineLink
                 description="「プレゼンで時間オーバー」解決するアプリを高校生が開発｜ 高校生新聞オンライン"
@@ -48,15 +49,15 @@ export default function Profile() {
                 href="https://youtu.be/JblLFCS-Eqw"
               />
             </TimeLineItem>
-            <TimeLineItem date="2022.8" title="全国専門学科情報科研究協議会  優秀賞">
+            <TimeLineItem date="2022.8" icon={faTrophy} title="全国専門学科情報科研究協議会  優秀賞">
               専門学科 情報科の設置校が集まって年に1度開催する研究協議会にて、プレゼン原稿表示アプリ「Presc」を発表しました。生徒表彰にて優秀賞を頂きました。
               <br />
             </TimeLineItem>
-            <TimeLineItem date="2023.1 ~" title="株式会社Wandarlust  Locket開発">
+            <TimeLineItem date="2023.1 ~" icon={faBriefcase} title="株式会社Wandarlust  Locket開発">
               位置情報共有アプリ「Locket」のフロントエンド（Flutter）チームに約2ヶ月間所属しました。主にチャット機能の開発に携わりました。
               <br />
             </TimeLineItem>
-            <TimeLineItem date="2023.4 ~" title="慶應義塾大学  環境情報学部" />
+            <TimeLineItem date="2023.4 ~" icon={faGraduationCap} title="慶應義塾大学  環境情報学部" />
           </Timeline>
 
           <h1 className={index.h1}>取得資格</h1>
