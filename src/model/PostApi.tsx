19f5d7f67a564d84a184e31638e0c2f7914bf45c
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-03-24 23:59:29 +0900

    Move file

--- src/model/PostApi.tsx
+++ src/model/PostApi.tsx
@@ -2,7 +2,7 @@ import path from "path";
 import fs from "fs";
 
 function fetchPostData(name: string) {
-  const fullPath = path.join(process.cwd(), "src/posts/", `${name}.md`);
+  const fullPath = path.join(process.cwd(), "posts/", `${name}.md`);
   return fs.readFileSync(fullPath, "utf8");
 }
 
