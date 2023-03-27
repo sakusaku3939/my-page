70bcbc9d8496d17e0b208c5fcdb5f56986bf7c2b
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-03-27 23:54:24 +0900

    Add index

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,10 @@
+import posts from "@/styles/posts.module.css";
+import common from "@/styles/common.module.css";
+const Index = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Index;
