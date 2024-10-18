90dc2efb970428a4002a428b95c25988853b71c4
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-10-18 16:53:14 +0900

    Add end date

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -20,8 +20,8 @@ const Index = () => {
       <section className={index.section}>
         <h1 className={index.h1}>経歴</h1>
         <Timeline>
-          <TimeLineItem date="2019.4 ~" icon={faGraduationCap} title="東京都立新宿山吹高校 情報科" />
-          <TimeLineItem date="2019.8 ~" icon={faBriefcase} title="FRCロボコンチーム BWW" open={false}>
+          <TimeLineItem date="2019.4 ~ 2023.3" icon={faGraduationCap} title="東京都立新宿山吹高校 情報科" />
+          <TimeLineItem date="2019.8 ~ 2022.3" icon={faBriefcase} title="FRCロボコンチーム BWW" open={false}>
             高1〜高3に世界最大級のロボコン FRC（FIRST Robotics
             Competition）のチーム「BWW」に初期メンバーとして活動しました。ロボットのプログラミングや公式Webサイト作成、YouTube動画編集、クラウドファンディング、ブログなどでの広報活動を行いました。
             <TimeLineLink
@@ -33,14 +33,16 @@ const Index = () => {
               href="https://www.youtube.com/@BWW8231"
             />
           </TimeLineItem>
-          <TimeLineItem date="2023.1 ~" icon={faBriefcase} title="株式会社Wanderlust Flutterエンジニア" open={false}>
+          <TimeLineItem date="2023.1 ~ 2023.3" icon={faBriefcase} title="株式会社Wanderlust Flutterエンジニア"
+                        open={false}>
             Flutterフロントエンドチームで約2ヶ月間、位置情報共有アプリ「Locket」の開発に従事しました。チャット機能やプロフィール画面などの新規開発や、チケット管理ツールを用いたスクラム開発を行いました。
           </TimeLineItem>
-          <TimeLineItem date="2023.4 ~" icon={faGraduationCap} title="慶應義塾大学 環境情報学部（SFC）" />
-          <TimeLineItem date="2024.8 ~" icon={faBriefcase} title="Gather Inc. 開発アルバイト" open={true}>
-            現在、Gather株式会社にてFlutterアプリの0→1開発に携わっています。技術総括のもと、Clean Architectureとチケット駆動によるアプリケーション開発を行っています。
+          <TimeLineItem date="2023.4 ~ 2027.3" icon={faGraduationCap} title="慶應義塾大学 環境情報学部（SFC）" />
+          <TimeLineItem date="2024.8 ~ 現在" icon={faBriefcase} title="Gather Inc. 開発アルバイト" open={false}>
+            現在、Gather株式会社にてFlutterアプリの0→1開発に携わっています。技術総括のもと、Clean
+            Architectureとチケット駆動によるアプリケーション開発を行っています。
           </TimeLineItem>
-          <TimeLineItem date="2023.4 ~" icon={faGraduationCap} title="村井合同研究会 d-hacks" open={true}>
+          <TimeLineItem date="2023.4 ~ 現在" icon={faGraduationCap} title="村井合同研究会 d-hacks" open={true}>
             学部1年の4月から村井合同研究会（RG）の中澤・大越研究室（d-hacks）に所属し、深層学習やCVに関する研究を行っています。
             <TimeLineLink
               description="d-hacks"
