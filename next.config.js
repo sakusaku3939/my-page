7e878cd624794f0e83c1a4a4873f264afcf500cc
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-12-14 00:00:54 +0900

    Add a publication

--- next.config.js
+++ next.config.js
@@ -1,17 +1,25 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true
   },
   i18n: {
-    locales: ['ja'],
-    defaultLocale: 'ja',
+    locales: ["ja"],
+    defaultLocale: "ja"
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: "/db/3643832.3661836",
+        destination: "/db/yololstm.pdf"
+      }
+    ];
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
