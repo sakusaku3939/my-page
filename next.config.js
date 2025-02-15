0d223a87ddbd846481aac39ce1cada896ebc9e12
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-02-15 16:20:33 +0900

    Add to db

--- next.config.js
+++ next.config.js
@@ -17,6 +17,10 @@ const nextConfig = {
       {
         source: "/db/3643832.3661836",
         destination: "/db/yololstm.pdf"
+      },
+      {
+        source: "/db/2024f-term-resume.pdf",
+        destination: "/db/2024f-term-resume.pdf"
       }
     ];
   }
