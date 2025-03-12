166f218258eb746d1afc39fc0e461af4f1aad5fe
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-03-13 02:18:42 +0900

    Add GA settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,10 @@ import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import { Router } from "next/router";
 import { Meta } from "@/components/atom/Meta/Meta";
+import Script from "next/script";
+import * as gtag from "src/lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 NProgress.configure({ showSpinner: false, speed: 300, minimum: 0.25 });
 
@@ -12,10 +16,38 @@ export default function App({ Component, pageProps }: AppProps) {
   Router.events.on("routeChangeComplete", () => NProgress.done());
   Router.events.on("routeChangeError", () => NProgress.done());
 
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `
+        }}
+      />
       <Meta />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
