50f5270c968fa89494ff3c4c1f7bc15f13dfc150
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-08-28 05:06:17 +0900

    Add placeholder component for mobile categories

--- src/components/molecule/MobileCategory/MobileCategory.tsx
+++ src/components/molecule/MobileCategory/MobileCategory.tsx
@@ -9,6 +9,9 @@ type Props = {
 
 const MobileCategory = ({ categories }: Props) => {
   const router = useRouter();
+  if (categories.length === 0) {
+    return <PlaceholderMobileCategory />;
+  }
   return <>
     <div className={category.tag}>
       <ul className={common.tag}>
@@ -17,4 +20,15 @@ const MobileCategory = ({ categories }: Props) => {
     </div>
   </>;
 };
+
+const PlaceholderMobileCategory = ({}) => {
+  return (
+    <div className={category.tag}>
+      <ul className={`${common.tag} ${category.placeholder}`}>
+        {Array(10).fill(0).map((_, i) => <li key={i}>&nbsp;</li>)}
+      </ul>
+    </div>
+  );
+};
+
 export default MobileCategory;
