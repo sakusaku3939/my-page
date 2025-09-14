066e8db15a660bbff991b3e63d3ba4d27c15c50f
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-09-15 01:37:46 +0900

    Add a page for displaying QR code

---
+++ src/pages/x/index.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import index from "@/styles/index.module.css";
+import { BackgroundGradientWrapper } from "@/components/atom/BackgroundGradientWrapper/BackgroundGradientWrapper";
+import Image from "next/image";
+
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>Aokiti | X</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <BackgroundGradientWrapper>
+        <div className={index.qrCodeWrapper}>
+          <div className={index.qrCode}>
+            <Image src="/images/qrcode.svg" alt="QR Code" sizes="100%" fill />
+          </div>
+          <p className={index.qrCodeText}>@aokiti_dev</p>
+        </div>
+      </BackgroundGradientWrapper>
+    </>
+  );
+};
+
+export default Index;
